a4591b634272b4d4f577742164815e028f7be7d0
macoyshev <make.oynoshev@gmail.com>
2021-08-06 17:42:15 +0700

    1212

--- commands/chooseRole.js
+++ commands/chooseRole.js
@@ -4,32 +4,46 @@ module.exports.run = (client, message, args, prefix) => {
     
     //get user who send command
     const id = message.author.id
-    const user = message.guild.members.cache.get(id)
-    
-    
-    message.guild.members.fetch()
-        .then(members => {
-            let guild_members = new Map() 
+    let user = message.guild.members.cache.get(id)
+    console.log(user);
+    let guild_members = new Map()
+    let current_guild
 
-            members.forEach(member => {
-                guild_members.set(member.user.username, member.user)
-            })
-        })
-        .catch()
+    //get map of guild members
+    current_guild = message.guild
+    current_guild.members.cache.forEach(guildMember => {
+        guild_members.set(guildMember.user.username, guildMember) 
+    })
+    
 
+    //work with role change
     message.guild.roles.fetch()
         .then(roles => {
             let roles_map = new Map()
+            let member_name
             
             //define map to easly find credit role
             roles.cache.forEach(role => {
                 roles_map.set(role.name, role)
             })
+
             if (!args[0]){
                 displayRoles(roles_map, message)
                 return;
             }
+            
+            if (args[1]){
+                if (guild_members.get(args[1])) {
+                    member_name = args[1]
+                }else {
+                    message.channel.send(`${member_name} is not guild member`)
+                    return    
+                }
+                user = guild_members.get(member_name)
+                console.log(user);
+            }
 
+            //set role to the user
             try {
                 let role = roles_map.get(args[0])
 
